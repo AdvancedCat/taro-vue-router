e8d82e37d9a32a9a4d4307793f0931db4552d6bd
advancedcat <wshx1938@163.com>
2022-10-25 15:13:25 +0800

    debugger for webpack5

--- config/index.js
+++ config/index.js
@@ -17,7 +17,13 @@ const config = {
     options: {}
   },
   framework: "vue3",
-  compiler: "webpack5",
+  compiler: {
+    type: "webpack5",
+    prebundle: {
+      timings: true,
+      exclude: ['@nutui/nutui-taro'],
+    },
+  },
   cache: {
     enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
   },
